2d2d9d2f5d44e75a97cb089a33bddd0c7085e04c
Constantin <CVennekel@gmail.com>
2021-05-03 17:31:16 +0200

    added missing provider

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
+          StreamProvider<Report>.value(value: Global.topicsRef.documentStream),
           StreamProvider<FirebaseUser>.value(value: AuthService().user)
         ],
         child: MaterialApp(
